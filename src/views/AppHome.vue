5a6ab3a56f9849492042d2a86ea61306e364aa7a
Nasrullayev Alisher <myemail9445@inbox.ru>
2023-02-21 09:17:25 +0500

    vuetify added

---
+++ src/views/AppHome.vue
@@ -0,0 +1,15 @@
+<template>
+  <app-form />
+</template>
+
+<script>
+  import AppForm from '../components/AppForm'
+
+  export default {
+    name: 'AppHome',
+
+    components: {
+      AppForm,
+    },
+  }
+</script>
